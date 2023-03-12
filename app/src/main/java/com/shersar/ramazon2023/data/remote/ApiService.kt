5e30ac9704ce70c1a4cfda1cca1cb0781a72eb50
Master <Jaxadev>
2023-03-13 00:50:28 +0500

    di and some dependencies added

--- app/src/main/java/com/shersar/ramazon2023/data/remote/ApiService.kt
+++ app/src/main/java/com/shersar/ramazon2023/data/remote/ApiService.kt
@@ -1,4 +1,8 @@
 package com.shersar.ramazon2023.data.remote
 
+import retrofit2.http.GET
+
 interface ApiService {
+
+
 }
---
+++ app/src/main/java/com/shersar/ramazon2023/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.shersar.ramazon2023.di
+
+import com.shersar.ramazon2023.data.remote.ApiService
+import com.shersar.ramazon2023.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, factory: GsonConverterFactory): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(client)
+            .addConverterFactory(factory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
